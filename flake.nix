d4e89f3d82152fcbb72b59e41c62f13dd2d3237f
Skyler Grey <skyler3665@gmail.com>
2023-06-07 22:44:42 +0200

    Add a basic flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      let
+        pnpm2nix = (import ./default.nix { inherit pkgs; });
+      in
+      {
+        inherit (pnpm2nix) mkPnpmPackage mkPnpmEnv;
+      });
+}
